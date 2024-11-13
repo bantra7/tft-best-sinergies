2d4697bd765519aa59eb22a293cfa9dba09100eb
bantra7 <renault.jbapt@gmail.com>
2024-11-13 20:42:11 +0100

    update script

--- download_data.py
+++ download_data.py
@@ -1,5 +1,4 @@
 import json
-import time
 from itertools import pairwise
 
 import pandas as pd
@@ -60,12 +59,16 @@ def main():
             print(f"Adding new champion : {champion.text}")
             champion_dict = {'Name': champion.text,
                              'Classes': [],
-                             'Origins': []
+                             'Origins': [],
+                             'Cost': 0
                              }
             r_champion = requests.get(f'{TFT_TACTICS_URL}{champion["href"]}')
             soup_champion = BeautifulSoup(r_champion.content, "lxml")
             # Récupération des traits du champion
             champion_traits = soup_champion.find_all("div", {"class": "ability-description-name"})
+            for champion_stats in soup_champion.find("ul", {"class": "stats-list"}).children:
+                if 'Cost' in champion_stats.text:
+                    champion_dict['Cost'] = int(champion_stats.text.split(' ')[-1])
             for champion_trait in champion_traits:
                 for champion_trait_data in pairwise(champion_trait.children):
                     if champion_trait_data[1].text == 'Class':
@@ -75,17 +78,19 @@ def main():
             champion_dict['Classes'] = '/'.join(champion_dict['Classes'])
             champion_dict['Origins'] = '/'.join(champion_dict['Origins'])
             champions.append(champion_dict)
-    traits_page_source = get_page_source(f'{TFT_TACTICS_URL}/db/classes/')
-    if traits_page_source:
-        soup_classes = BeautifulSoup(traits_page_source, 'html.parser')
+    classes_page_source = get_page_source(f'{TFT_TACTICS_URL}/db/classes/')
+    if classes_page_source:
+        soup_classes = BeautifulSoup(classes_page_source, 'html.parser')
         classes_list = [class_data.text for class_data in soup_classes.find_all("div", {"class": "d-none d-md-block"})]
         classes_bonus_numbers = [tuple(int(classes_bonus_number.text) for classes_bonus_number in
                                        class_bonus_data.find_all("div", {"class": "table-bonus-count"})) for
                                  class_bonus_data in
                                  soup_classes.find_all("div", {"class": "table-bonus-list"})]
         classes_list = list({(classes_list[i], classes_bonus_numbers[i]) for i in range(0, len(classes_list))})
-        r_origins = requests.get(f'{TFT_TACTICS_URL}/db/origins/')
-        soup_origins = BeautifulSoup(r_origins.content, "lxml")
+        traits_dict['classes'] = {class_tuple[0]: list(class_tuple[1]) for class_tuple in classes_list}
+    origins_page_source = get_page_source(f'{TFT_TACTICS_URL}/db/origins/')
+    if origins_page_source:
+        soup_origins = BeautifulSoup(origins_page_source, "lxml")
         origins_list = [origin_data.text for origin_data in
                         soup_origins.find_all("div", {"class": "d-none d-md-block"})]
         origins_bonus_numbers = [tuple(int(origins_bonus_number.text) for origins_bonus_number in
@@ -93,7 +98,6 @@ def main():
                                  origin_bonus_data in
                                  soup_origins.find_all("div", {"class": "table-bonus-list"})]
         origins_list = list({(origins_list[i], origins_bonus_numbers[i]) for i in range(0, len(origins_list))})
-        traits_dict['classes'] = {class_tuple[0]: list(class_tuple[1]) for class_tuple in classes_list}
         traits_dict['origins'] = {origin_tuple[0]: list(origin_tuple[1]) for origin_tuple in origins_list}
     # Save Data
     df_champions = pd.json_normalize(champions)
